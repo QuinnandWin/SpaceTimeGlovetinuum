2a55924c6c7be300c4a9a9c8f625521dc80b2b66
Quinn Nolan <quinn.nolan@students.plymouth.ac.uk>
2018-02-03 18:42:22 +0000

    Created basic player 'death' and teleport

---
+++ Project/Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject teleportLocation;
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            print("player has died");
+            player.transform.position = teleportLocation.transform.position;
+        }
+    }
+
+}
