c79800bcc80f85e14e906ba2c852f5d4479ccc35
Quinn Nolan <quinn.nolan@students.plymouth.ac.uk>
2018-02-04 16:38:57 +0000

    Created door that opens to player key

---
+++ Project/Assets/Scripts/UnlockDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoor : MonoBehaviour {
+
+    [SerializeField]
+    protected GameObject door;
+
+    void OnTriggerEnter(Collider key)
+    {
+        if (key.gameObject.tag == "Key")
+        {
+            print("key has unlocked door");
+            door.SetActive(false);
+            Destroy(key.gameObject);
+        }
+    }
+}
