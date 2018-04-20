eb3a55d30a42bab5e6e145ecb35811494aebf62e
QuinnandWin <quinn.nolan@students.plymouth.ac.uk>
2018-04-20 14:24:00 +0100

    Further created level 2

---
+++ Project/Assets/Scripts/ActivateMovingCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateMovingCamera : MonoBehaviour {
+
+    [SerializeField]
+    bool activate = true;
+    [SerializeField]
+    GameObject cameraObject;
+    [SerializeField]
+    Transform startNode;
+    [SerializeField]
+    float xRotation;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player" && activate == true)
+        {
+            cameraObject.GetComponent<SmoothCamera>().enabled = false;
+            cameraObject.GetComponent<MoveBetweenMultipleNodes>().enabled = true;
+            cameraObject.transform.position = startNode.position;
+            cameraObject.transform.rotation = Quaternion.Euler(xRotation, cameraObject.transform.rotation.y, cameraObject.transform.rotation.z);
+        }
+
+        else if (player.gameObject.tag == "Player" && activate == false)
+        {
+            cameraObject.GetComponent<SmoothCamera>().enabled = true;
+            cameraObject.GetComponent<MoveBetweenMultipleNodes>().enabled = false;
+        }
+    }
+}
